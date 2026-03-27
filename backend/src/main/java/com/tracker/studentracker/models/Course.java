d005f413bc8e2d0b1f2309671f042690eaadb84e
Nandana-js <nandanasjayakumar@gmail.com>
2026-03-27 19:37:51 +0530

    feat: add Student, User, and Course entity models

---
+++ backend/src/main/java/com/tracker/studentracker/models/Course.java
@@ -0,0 +1,8 @@
+package com.tracker.studentracker.models;
+
+public class Course {
+    private int id;
+    private String name;
+    private String description;
+}
+
---
+++ backend/src/main/java/com/tracker/studentracker/models/Student.java
@@ -0,0 +1,31 @@
+package com.tracker.studentracker.models;
+
+public class Student {
+    public enum Status {
+        PENDING,
+        APPROVED,
+        REJECTED
+
+    }
+    private int studentId;
+    private int userId;
+    private String studentNumber;
+    private int majorCourseId;
+    private Status status;
+
+    public int getStudentId(){return this.studentId;}
+    public void setStudentId(int studentId){this.studentId = studentId;}
+
+    public int getUserId(){return this.userId;}
+    public void setUserId(int userId){this.userId = userId;}
+
+    public String getStudentNumber(){return this.studentNumber;}
+    public void setStudentNumber(String studentNumber){this.studentNumber = studentNumber;}
+
+    public int getMajorCourseId(){return this.majorCourseId;}
+    public void setMajorCourseId(int majorCourseId){this.majorCourseId = majorCourseId;}
+
+    public Status getStatus(){return this.status;}
+    public void setStatus(Status status){this.status = status;}
+
+}
