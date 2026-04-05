70f8f9b6e1f5c3523c3786cd229af8b37e5038ad
Fadil Rahman <87557055+fadil4u@users.noreply.github.com>
2026-04-05 18:13:06 +0530

    delete old academic calendar

--- academic_calender/src/main/java/com/jeenify/academic_calender/repository/AcademicEventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jeenify.academic_calender.repository;
-
-import com.jeenify.academic_calender.model.AcademicEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AcademicEventRepository extends JpaRepository<AcademicEvent, Long> {
-}
