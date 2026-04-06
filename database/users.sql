0108bb0f8561e9b3d37957f26b7f259702f00a07
Ashiqalink <ashiqali2004mji@gmail.com>
2026-04-07 04:13:37 +0530

    .

--- database/users.sql
+++ database/users.sql
@@ -21,11 +21,11 @@ CREATE TABLE teachers (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 INSERT INTO users (name, email, password_hash, role) VALUES
-('Admin User', 'admin@college.com', 'hash123', 'ADMIN'),
-('John Student', 'john@student.com', 'hash123', 'STUDENT'),
-('Aisha Student', 'aisha@student.com', 'hash123', 'STUDENT'),
-('Dr Smith', 'smith@college.com', 'hash123', 'TEACHER'),
-('Prof Kumar', 'kumar@college.com', 'hash123', 'TEACHER');
+('Admin User', 'admin@college.com', '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'ADMIN'),
+('John Student', 'john@student.com', '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'STUDENT'),
+('Aisha Student', 'aisha@student.com', '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'STUDENT'),
+('Dr Smith', 'smith@college.com', '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'TEACHER'),
+('Prof Kumar', 'kumar@college.com', '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'TEACHER');
 INSERT INTO students (user_id, student_number, major_course_id, status) VALUES
 (2, 'STU001', 1, 'approved'),
 (3, 'STU002', 1, 'pending');
